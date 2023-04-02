fb7fe733b7a150a5e1bdb1875c6e7fb6a07efdae
Cheetah83 <ssenyange86@yahoo.com>
2023-04-02 08:08:37 +0300

    added simple database

--- products.js
+++ products.js
@@ -4,14 +4,14 @@ const products = [
     title : "iPhone 14 Pro Max",
     price : 800,
     desc : "The range-topping iPhone 13 Pro and iPhone 13 Pro Max — the third consecutive iPhone duo to rock the “Pro” tag — are, yet again, Apple’s most feature-packed phones to date. The successors to the iPhone 12 Pro and iPhone 12 Pro Max deliver consequential hardware improvements while retaining the same design and luxurious workmanship. In addition to the mandatory chip upgrade, new features include a ProMotion Super Retina XDR display with a high refresh rate, and an even better camera system.",
-    img : "https://s13emagst.akamaized.net/products/48661/48660941/images/res_a5ab29f33a928c44de39bf224a954562.jpg?width=450&height=450&hash=7FF12066C48E74D5BE4E3FBD03D683DA"
+    img : "https://i0.wp.com/abizot.com.ng/wp-content/uploads/2022/09/iPhone-14-Pro-Max-512GB.png?fit=770%2C770&ssl=1"
   },
   {
     id : 2,
     title : "Apple iPhone 12 Pro Max, 128GB, 5G, Gold",
     price : 750,
     desc : "The product is refurbished, fully functional, and in good condition. Backed by the 90-day Amazon Renewed Guarantee. This pre-owned product has been professionally inspected, tested and cleaned by Amazon qualified vendors. It is not certified by Apple. This product is in Good condition. The screen has no scratches; body shows light scratches barely visible from 12 inches away.",
-    img : "https://s13emagst.akamaized.net/products/33600/33599417/images/res_981b7018da3655425326dd5f878a5252.jpg"
+    img : "https://www.apple.com/newsroom/images/product/iphone/lifestyle/Apple-iPhone-14-iPhone-14-Plus-yellow-2up-230307_inline.jpg.large.jpg"
   },
   {
     id : 3,
@@ -25,7 +25,7 @@ const products = [
     title : "iPhone 14",
     price : 1500,
     desc : "A new Pro-level Main camera and improved image processing for even more sensational shots in all kinds of light",
-    img : "https://s13emagst.akamaized.net/products/48592/48591240/images/res_a4e5068cf223ad7dd7aac9ad750a6760.jpg"
+    img : "https://apexapr.mu/sites/default/files/product-images-2022-10/MPU63_0.jpeg"
   }
 ]
 module.exports = products;
