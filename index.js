938b1490e473f623a06b2a568d0598dc9bedc524
Cheetah83 <ssenyange86@yahoo.com>
2023-04-02 16:52:34 +0300

    added footer connected the application mongoDB and also worked on authentication of the user

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require('mongoose');
+const register = require("./routes/register");
+const login = require("./routes/login");
 
 const products = require('./products');
 
 const app = express();
 
+require("dotenv").config()
+
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/register", register);
+app.use("/api/login", login);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Yachi's Online Store")
@@ -15,5 +23,12 @@ app.get("/products", (req, res) => {
   res.send(products)
 })
 const PORT = process.env.PORT || 3000;
+const uri = process.env.DB_URI;
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connection successful"))
+.catch((err) => console.log("MongoDB connection failed", err.message));
